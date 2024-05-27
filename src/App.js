7ee19931078e6a7a4abf66ccfd448ec68dd7ea6d
Sarah <125749615+sarahtoninwrites@users.noreply.github.com>
2024-05-27 17:13:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   return (
     <div className="App">
       <Game />
+      <footer class="text-center justify-center items-center mt-9">
+        by <a href="https://github.com/sarahtoninwrites"> <strong>sarahtoninwrites</strong></a>
+      
+      </footer>
     </div>
+
   );
 }
 
