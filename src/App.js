b6b33c3d593bf9c9536c1195d0eb5cf8a8610344
Sarah <125749615+sarahtoninwrites@users.noreply.github.com>
2024-05-28 10:40:55 +0530

    mode select

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Game from './components/game';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-center justify-center items-center">
+      <h1 class="text-4xl text-center justify-center items-center mt-9 font-serif" >Tic Tac Toe</h1>
+      <button class="btn text-center justify-center items-center mt-6"> <a href="index.html">Mode Select</a></button>
       <Game />
       <footer class="text-center justify-center items-center mt-9">
         by <a href="https://github.com/sarahtoninwrites"> <strong>sarahtoninwrites</strong></a>
